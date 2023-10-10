89e71ed7a36bd8b2db03bb34497202a7b516bba3
Vlatko Juric <juricv78gmail.com>
2023-10-10 21:36:33 +0200

    js fetch exercise done,unfortunatelly without the bonus,could not find out how,did not want to look at solution lol

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData(url) {
+  const response = await fetch(url);
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+}
 
-fetchData();
+fetchData(url);
